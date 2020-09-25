f95895a39d111b65b241c9f49cd77a53a8dc52bc
iii <miselrkba@gmail.com>
2020-09-25 18:15:15 +0200

    media queries for smallest screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav>
-        <ul>
+        <ul className='navigation'>
           <li>
             <Link
               className="link"
--- src/components/work.js
+++ src/components/work.js
@@ -8,7 +8,7 @@ class Work extends Component {
   render() {
     return (
       <div>
-        <h1 className="heading">
+        <h1 className="header-projects">
           <Fade bottom cascade>
             Projects.
           </Fade>
