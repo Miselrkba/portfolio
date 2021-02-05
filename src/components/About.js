afa2a675358ce28352a5e6e7b2677fa08f8fa34c
iii <miselrkba@gmail.com>
2021-02-05 21:43:11 +0100

    fix: eslint airbnb cleanup & refactor

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,25 @@
-import React, { Component } from "react";
-import Fade from "react-reveal/Fade";
-import data from "../yourdata";
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import data from '../yourdata';
 
-import image from "../images/portret06.jpg";
+import image from '../images/portret06.jpg';
 
-class About extends Component {
-  
-  render() {
-    return (
-      <div className="about">
-        <div className="about-content">
-          <h1>
-            <Fade bottom cascade>
-              About.
-            </Fade>
-          </h1>
-          <Fade bottom>
-            <p>{data.abouttext}</p>
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-content">
+        <h1>
+          <Fade bottom cascade>
+            About.
           </Fade>
-        </div>
-        <img className="about-image" src={image} alt="about"></img>
+        </h1>
+        <Fade bottom>
+          <p>{data.abouttext}</p>
+        </Fade>
       </div>
-    );
-  }
-}
+      <img className="about-image" src={image} alt="about" />
+    </div>
+  );
+};
 
 export default About;
--- src/yourdata.js
+++ src/yourdata.js
@@ -1,15 +1,14 @@
-
 export default {
   name: 'Michael Huna',
   headerTagline: [
-    //Line 1 For Header
+    // Line 1 For Header
     'Hello! 👋 I am Michael, a software developer.',
-    //Line 2 For Header
+    // Line 2 For Header
     'Welcome to my website. Feel free to check out my projects below...',
-    //Line 3 For Header
+    // Line 3 For Header
     '',
   ],
-  //Contact Email
+  // Contact Email
   contactEmail: 'michaelhuna@yahoo.com',
   // Add Your About Text Here
   abouttext:
@@ -18,11 +17,12 @@ export default {
   projects: [
     {
       id: 1,
-      title: 'Searchable CRUD Table', //Project Title - Add Your Project Title Here
+      title: 'Searchable CRUD Table', // Project Title - Add Your Project Title Here
       service: 'Add new users, Edit users, Delete users, Search users', // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
-      imageSrc: `https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80`,
-      //Project URL - Add Your Project Url Here
+      // Project Image - Add Your Project Image Here
+      imageSrc:
+        'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+      // Project URL - Add Your Project Url Here
       url: 'https://searchable-crud-table.netlify.app/#',
       gitUrl:
         'https://github.com/Miselrkba/react-reusable-searchable-crud-table',
@@ -31,7 +31,8 @@ export default {
       id: 2,
       title: 'Dedinky Chat App',
       service: 'Chat for villages in Ružomberok region',
-      imageSrc: `https://image.freepik.com/free-photo/cityscape-icon-symbol-web-element_1150-1791.jpg`,
+      imageSrc:
+        'https://image.freepik.com/free-photo/cityscape-icon-symbol-web-element_1150-1791.jpg',
       url: 'https://dedinky.netlify.app/',
       gitUrl: 'https://github.com/Miselrkba/dediny-barebone/tree/newfeatures',
     },
@@ -39,21 +40,22 @@ export default {
       id: 3,
       title: 'XpressLeads Sales Helper',
       service: 'Productivity app for sales people. Data fetched from API. ',
-      imageSrc: `https://images.unsplash.com/photo-1507099985932-87a4520ed1d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80`,
+      imageSrc:
+        'https://images.unsplash.com/photo-1507099985932-87a4520ed1d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80',
       url: 'https://xpressleads.netlify.app/',
       gitUrl: 'https://github.com/Miselrkba/leads',
     },
     {
       id: 4,
-      title: 'Admin Dashboard Table', //Project Title - Add Your Project Title Here
+      title: 'Admin Dashboard Table', // Project Title - Add Your Project Title Here
       service: 'Table with sorting, expandable and selectable rows', // Add Your Service Type Here
-      //Project Image - Add Your Project Image Here
-      imageSrc: `https://image.freepik.com/free-vector/multitasking-concept-illustration_52683-32123.jpg`,
-      //Project URL - Add Your Project Url Here
+      // Project Image - Add Your Project Image Here
+      imageSrc:
+        'https://image.freepik.com/free-vector/multitasking-concept-illustration_52683-32123.jpg',
+      // Project URL - Add Your Project Url Here
       url: 'https://admin-dashboard-table.netlify.app/',
       gitUrl: 'https://github.com/Miselrkba/admin-table',
     },
-
   ],
   social: [
     // Add Or Remove The Link Accordingly
