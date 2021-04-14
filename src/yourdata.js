b43d4aa5b8fc6edabd48e85636f380487173f758
iii <miselrkba@gmail.com>
2021-04-14 14:56:53 +0200

    fix: updated images and projects

--- src/yourdata.js
+++ src/yourdata.js
@@ -1,28 +1,34 @@
+import adminImg from './images/admin.jpg';
+import chatImg from './images/chat.jpg';
+import laptopImg from './images/laptop.jpg';
+import productivityImg from './images/productivity.jpg';
+import malconfImg from './images/malconf.jpg';
+import spartanImg from './images/spartan.jpg';
+
 export default {
   name: 'Michael Huna',
   headerTagline: [
-    // Line 1 For Header
+    // line 1 for header
     'Hello! 👋 I am Michael, a software developer.',
-    // Line 2 For Header
+    // line 2 For header
     'Welcome to my website. Feel free to check out my projects below...',
-    // Line 3 For Header
+    // line 3 For header
     '',
   ],
-  // Contact Email
+  // contact email
   contactEmail: 'michaelhuna@yahoo.com',
-  // Add Your About Text Here
+  // add your about text here
   abouttext:
     'I build awesome web experiences using React.js, JavaScript ES6, Html5 and Css3. I provide technical, creative, and business insights to all projects I work on. Build for flexibility and customization from the start. I plan responsive software for all mediums, screen sizes and device types.',
-  // Change Projects Here
+  // change projects here
   projects: [
     {
       id: 1,
-      title: 'Searchable CRUD Table', // Project Title - Add Your Project Title Here
-      service: 'Add new users, Edit users, Delete users, Search users', // Add Your Service Type Here
-      // Project Image - Add Your Project Image Here
-      imageSrc:
-        'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-      // Project URL - Add Your Project Url Here
+      title: 'Searchable CRUD Table',
+      service: 'Add new users, Edit users, Delete users, Search users',
+      // add project img here
+      imageSrc: laptopImg,
+      // add project url here
       url: 'https://searchable-crud-table.netlify.app/#',
       gitUrl:
         'https://github.com/Miselrkba/react-reusable-searchable-crud-table',
@@ -31,8 +37,7 @@ export default {
       id: 2,
       title: 'Dedinky Chat App',
       service: 'Chat for villages in Ružomberok region',
-      imageSrc:
-        'https://image.freepik.com/free-photo/cityscape-icon-symbol-web-element_1150-1791.jpg',
+      imageSrc: chatImg,
       url: 'https://dedinky.netlify.app/',
       gitUrl: 'https://github.com/Miselrkba/dediny-barebone/tree/newfeatures',
     },
@@ -40,25 +45,43 @@ export default {
       id: 3,
       title: 'XpressLeads Sales Helper',
       service: 'Productivity app for sales people. Data fetched from API. ',
-      imageSrc:
-        'https://images.unsplash.com/photo-1507099985932-87a4520ed1d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=80',
+      imageSrc: productivityImg,
       url: 'https://xpressleads.netlify.app/',
       gitUrl: 'https://github.com/Miselrkba/leads',
     },
     {
       id: 4,
-      title: 'Admin Dashboard Table', // Project Title - Add Your Project Title Here
-      service: 'Table with sorting, expandable and selectable rows', // Add Your Service Type Here
-      // Project Image - Add Your Project Image Here
-      imageSrc:
-        'https://image.freepik.com/free-vector/multitasking-concept-illustration_52683-32123.jpg',
-      // Project URL - Add Your Project Url Here
+      title: 'Admin Dashboard Table',
+      service: 'Table with sorting, expandable and selectable rows',
+      // add project img here
+      imageSrc: adminImg,
+      // add project url here
       url: 'https://admin-dashboard-table.netlify.app/',
       gitUrl: 'https://github.com/Miselrkba/admin-table',
     },
+    {
+      id: 5,
+      title: 'Malconf Website',
+      service: 'Animated website for Malconf - Malware configuration parser',
+      // add project img here
+      imageSrc: malconfImg,
+      // add project url here
+      url: 'https://malconf.netlify.app/',
+      gitUrl: 'https://github.com/Miselrkba/react-malconf-client',
+    },
+    {
+      id: 6,
+      title: 'Leonides Real Estate Company',
+      service: 'Beautiful real estate business landing page ',
+      // add project img here
+      imageSrc: spartanImg,
+      // add project url here
+      url: 'https://leonides.netlify.app/',
+      gitUrl: 'https://github.com/Miselrkba/leonides-website-react',
+    },
   ],
   social: [
-    // Add Or Remove The Link Accordingly
+    // add or remove link to social
     { name: 'Github', url: 'https://github.com/Miselrkba' },
   ],
 };
